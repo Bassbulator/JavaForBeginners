096786454b59f37608392267bf4d240698e04723
Bassbulator <belikovm007@gmail.com>
2021-04-01 18:56:14 +0300

    add Variables

---
+++ src/com/company/Variables.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Variables {
+    public static void main(String[] args) {
+        double a = 123.12;
+        byte number = 127;
+        System.out.println(a);
+        System.out.println(number);
+    }
+}
