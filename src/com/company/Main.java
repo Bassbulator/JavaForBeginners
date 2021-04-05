28d9a9437a41fc94dce7fb80210d7ca687511aba
Bassbulator <belikovm007@gmail.com>
2021-04-05 14:17:37 +0300

    Homework 3 Arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,15 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println ("Hello, world!");
-        System.out.println ("Hello, world!");
+        int[] array = {1,2,3,4,5};
+        int a = array[0]; // присвоилась 1
+        array[0] = array[array.length -1]; // первый эл-т стал посл
+        array[array.length -1] = a; // сохранили в посл эл что было в первом
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
-
 }
